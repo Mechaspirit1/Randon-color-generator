3a399e20c32c40b53f184e80de20adca80da3149
Mechaspirit1 <pabloloschi200@gmail.com>
2025-10-16 16:40:24 -0300

    Enhance RGB color generation with brightness check
    Added brightness calculation to adjust text color based on background color.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const title = document.querySelector("h1");
 const btn = document.querySelector("#btn");
 const rgb = document.querySelector("#sub");
 
@@ -7,11 +8,25 @@ btn.addEventListener("click", ()=>{
     let num2 = Math.floor(Math.random()*256);
     let num3 = Math.floor(Math.random()*256);
 
+    //calculo para determinar se a cor de fundo é clara ou excura
+    let bright = num1*0.2126 + num2*0.7152 + num3*0.0722
+
     console.log(num1);
     console.log(num2);
     console.log(num3);
+    console.log(bright);
 
     rgb.textContent = `rgb(${num1}, ${num2}, ${num3})` //muda o conteudo do h2 para o valor rgb
     document.body.style.backgroundColor = `rgb(${num1}, ${num2}, ${num3})` //muda a cor de fundo da pagina
+
+    //se o calculo resultar em um numero menor que 128 a cor é considerada escura e a cor do text é atualizada de acordo
+    if(bright < 128){
+        title.style.color = "white";
+        rgb.style.color = "white";
+    }
+    else{
+        title.style.color = "black";
+        rgb.style.color="black";
+    }
 
 })
